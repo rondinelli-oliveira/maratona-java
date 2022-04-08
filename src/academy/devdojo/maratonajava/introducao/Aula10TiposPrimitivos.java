c3a5878ec2525252b29a62e348189de8e03a0c41
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-08 12:40:12 -0300

    aula-11 - Tipos primitivos pt 02 - declaração e tamanho em memória.

--- src/academy/devdojo/maratonajava/introducao/Aula10TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula10TiposPrimitivos.java
@@ -3,10 +3,22 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula10TiposPrimitivos {
     public static void main(String[] args) {
         //Tipos primitivos são os que armazenam em memória valores mais simples
-        //int, double, float, char, bute, short, long, boolean
+        //int, double, float, char, byte, short, long, boolean
         //por convenção as variáveis devem ser iniciadas com letra minúscula e se for composta
         //a segunda palavras começa com letra maiúscula
         int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDoule = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'R';
+
+
         System.out.println(idade);
+        System.out.println(verdadeiro);
+        System.out.println(caractere);
     }
 }
