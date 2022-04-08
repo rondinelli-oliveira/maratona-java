9c402c658e5dae86aad291972e33e43812fe92e7
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-08 20:32:37 -0300

    aula-19 - Operadores pt 05 - Atribuição.

--- src/academy/devdojo/maratonajava/introducao/Aula15Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula15Operadores.java
@@ -50,7 +50,26 @@ public class Aula15Operadores {
         float valorPlayStation = 5000F;
         boolean isPlayStationCincoCompravel = valorTotalContaCorrente > valorPlayStation || valorTotalContaPoupanca > valorPlayStation;
 
-        System.out.println("PlayStationCincoCompravel"+ isPlayStationCincoCompravel);
+        System.out.println("PlayStationCincoCompravel" + isPlayStationCincoCompravel);
+
+        //Operadores de atribuição
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        //++ --
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        int contador2 = 0;
+        System.out.println(contador);
+        System.out.println(++contador2);
     }
 
 }
