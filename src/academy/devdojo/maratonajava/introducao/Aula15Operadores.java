0916c13023e2f07abe25dd87bc9d2fd220060b4e
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-08 15:18:14 -0300

    aula-15 - Operadores pt 01 - Aritiméticos.

---
+++ src/academy/devdojo/maratonajava/introducao/Aula15Operadores.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+// + - / *
+public class Aula15Operadores {
+    public static void main(String[] args) {
+
+        int numero01 = 10;
+        int numero02 = 20;
+
+        System.out.println(numero02+numero01);
+        System.out.println(numero02-numero01);
+        System.out.println(numero02/numero01);
+        System.out.println(numero02*numero01);
+    }
+
+}
