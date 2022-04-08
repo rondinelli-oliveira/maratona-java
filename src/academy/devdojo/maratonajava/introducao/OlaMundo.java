3dcfaac8236ed087a02ebf44579321aa362bb148
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-08 11:33:28 -0300

    aula-09 - Comentários.

---
+++ src/academy/devdojo/maratonajava/introducao/OlaMundo.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
+public class OlaMundo {
+    //isso é um comentário de uma linha
+
+    /*
+        Isso é um comentário
+        de multiplas
+        linha
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!");
+        System.out.println("Hello world!");
+    }
+}
