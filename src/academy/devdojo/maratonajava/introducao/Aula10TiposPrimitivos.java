4470beef385611013f2fcea119bbde83fc16a7e6
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-08 12:23:41 -0300

    aula-10 - Tipos primitivos pt 01 - convenções de variáveis.

---
+++ src/academy/devdojo/maratonajava/introducao/Aula10TiposPrimitivos.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula10TiposPrimitivos {
+    public static void main(String[] args) {
+        //Tipos primitivos são os que armazenam em memória valores mais simples
+        //int, double, float, char, bute, short, long, boolean
+        //por convenção as variáveis devem ser iniciadas com letra minúscula e se for composta
+        //a segunda palavras começa com letra maiúscula
+        int idade = 10;
+        System.out.println(idade);
+    }
+}
