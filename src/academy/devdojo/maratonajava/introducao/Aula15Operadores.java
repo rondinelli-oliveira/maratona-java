8a4eb8416aedd7be50079714898e68842cbadfce
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-08 16:10:22 -0300

    aula-16 - Operadores pt 02 - Relacionais.

--- src/academy/devdojo/maratonajava/introducao/Aula15Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula15Operadores.java
@@ -1,16 +1,35 @@
 package academy.devdojo.maratonajava.introducao;
 
-// + - / *
 public class Aula15Operadores {
     public static void main(String[] args) {
-
+        // + - / *
         int numero01 = 10;
         int numero02 = 20;
 
-        System.out.println(numero02+numero01);
-        System.out.println(numero02-numero01);
-        System.out.println(numero02/numero01);
-        System.out.println(numero02*numero01);
+        System.out.println(numero02 + numero01);
+        System.out.println(numero02 - numero01);
+        System.out.println(numero02 / numero01);
+        System.out.println(numero02 * numero01);
+
+        // % resto
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // operadores de comparação
+        // < > <= >= ==(igual) !=(diferente)
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteVinte = 10 != 20;
+        boolean isDezDiferenteDez = 10 != 10;
+
+        System.out.println("DezMaiorQueVinte: " + isDezMaiorQueVinte);
+        System.out.println("DezMenorQueVinte: " + isDezMenorQueVinte);
+        System.out.println("DezIgualVinte: " + isDezIgualVinte);
+        System.out.println("DezIgualDez: " + isDezIgualDez);
+        System.out.println("DezDiferenteVinte: " + isDezDiferenteVinte);
+        System.out.println("DezDiferenteDez: " + isDezDiferenteDez);
     }
 
 }
