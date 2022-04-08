a6493e6fd5302b2699a6363b4d390e3a52454951
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-08 16:43:13 -0300

    aula-17 - Operadores pt 03 - Lógicos AND.

--- src/academy/devdojo/maratonajava/introducao/Aula15Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula15Operadores.java
@@ -19,6 +19,8 @@ public class Aula15Operadores {
         // < > <= >= ==(igual) !=(diferente)
         boolean isDezMaiorQueVinte = 10 > 20;
         boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezMaiorIgualQueVinte = 10 >= 20;
+        boolean isDezMenorIgualQueVinte = 10 <= 20;
         boolean isDezIgualVinte = 10 == 20;
         boolean isDezIgualDez = 10 == 10;
         boolean isDezDiferenteVinte = 10 != 20;
@@ -26,10 +28,22 @@ public class Aula15Operadores {
 
         System.out.println("DezMaiorQueVinte: " + isDezMaiorQueVinte);
         System.out.println("DezMenorQueVinte: " + isDezMenorQueVinte);
+        System.out.println("DezMaiorIgualQueVinte: " + isDezMaiorIgualQueVinte);
+        System.out.println("DezMenorIgualQueVinte: " + isDezMenorIgualQueVinte);
         System.out.println("DezIgualVinte: " + isDezIgualVinte);
         System.out.println("DezIgualDez: " + isDezIgualDez);
         System.out.println("DezDiferenteVinte: " + isDezDiferenteVinte);
         System.out.println("DezDiferenteDez: " + isDezDiferenteDez);
+
+        // operadores de lógicos
+        // && (AND) || (OU) ! (NOT)
+        int idade = 35;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade >= 30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta" + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta" + isDentroDaLeiMenorQueTrinta);
     }
 
 }
