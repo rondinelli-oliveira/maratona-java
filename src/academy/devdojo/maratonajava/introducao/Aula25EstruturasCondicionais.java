406bef64f3284aea5b66b556cd476b997e8c7907
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-09 18:20:34 -0300

    aula-25 - Estruturas Condicionais pt 06 - Switch.

---
+++ src/academy/devdojo/maratonajava/introducao/Aula25EstruturasCondicionais.java
@@ -0,0 +1,49 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula25EstruturasCondicionais {
+    public static void main(String[] args) {
+        //Imprima o dia da semana considerando o 01 DOMINGO
+
+        byte dia = 5;
+
+        // char, int, byte, short, enum, String
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-Feira");
+                break;
+            case 3:
+                System.out.println("Terça-Feira");
+                break;
+            case 4:
+                System.out.println("Quarte-Feira");
+                break;
+            case 5:
+                System.out.println("Quinta-Feira");
+                break;
+            case 6:
+                System.out.println("Sexta-Feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                break;
+        }
+
+        char sexo = 's';
+        switch (sexo) {
+            case 'M':
+                System.out.println("Homem");
+                break;
+            case 'F' :
+                System.out.println("Mulher");
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
+    }
+}
