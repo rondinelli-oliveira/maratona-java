29303442c18c2d36012546e9ff9dc4e3686987bd
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-11 13:17:06 -0300

    aula-38 - Arrays Multidimensionais pt 03 - Inicialização.

---
+++ src/academy/devdojo/maratonajava/introducao/Aula38ArraysMultidimensionais.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula38ArraysMultidimensionais {
+    public static void main(String[] args) {
+        //dias
+        //meses
+        int[][] arrayInt = new int[3][3];
+        arrayInt[0] = new int[2];
+        arrayInt[1] = new int[3];
+        arrayInt[2] = new int[6];
+
+
+        for (int[] arrayBase : arrayInt) {
+            System.out.println("\n--------------------");
+            for (int num : arrayBase) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
