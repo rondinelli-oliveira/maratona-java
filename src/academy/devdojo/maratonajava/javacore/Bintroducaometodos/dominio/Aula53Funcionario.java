adc9d3c06e5aa3ce90a015d36c3861e1e22bb859
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-12 18:08:52 -0300

    aula-56 - Orientação Objetos - Modificador de acesso private, get e set pt 03.

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Aula53Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Aula53Funcionario.java
@@ -1,9 +1,38 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Aula53Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public double getMedia() {
+        return media;
+    }
 
     public void imprimedados() {
         System.out.println(this.nome);
@@ -22,7 +51,6 @@ public class Aula53Funcionario {
         if (salarios == null) {
             return;
         }
-        double media = 0;
 
         for (double salario : salarios) {
             media += salario;
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/Aula53FuncionarioTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/Aula53FuncionarioTest01.java
@@ -5,10 +5,11 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Aula53Fu
 public class Aula53FuncionarioTest01 {
     public static void main(String[] args) {
         Aula53Funcionario funcionario = new Aula53Funcionario();
-        funcionario.nome = "Seya";
-        funcionario.idade = 23;
-        funcionario.salarios = new double[]{1200, 987.32, 2000};
+        funcionario.setNome("Seya");
+        funcionario.setIdade(23);
+        funcionario.setSalarios(new double[]{1200, 987.32, 2000});
 
         funcionario.imprimedados();
+        System.out.println("\nMédia" + funcionario.getMedia());
     }
 }
