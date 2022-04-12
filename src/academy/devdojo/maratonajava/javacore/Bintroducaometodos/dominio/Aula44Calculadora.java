d2e2cfa8bac076a1fe81f7e6ea5b5984be4591c6
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-12 16:24:06 -0300

    aula-52 - Orientação Objetos - Métodos pt 09 - Varargs.

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Aula44Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Aula44Calculadora.java
@@ -49,4 +49,20 @@ public class Aula44Calculadora {
         System.out.println("Numero1 " + numero1);
         System.out.println("Numero2 " + numero2);
     }
+
+    public void somaArray(int[] numeros) {
+        int soma = 0;
+        for (int num : numeros) {
+            soma += num;
+        }
+        System.out.println(soma);
+    }
+
+    public void somaVarArgs(int... numeros) {
+        int soma = 0;
+        for (int num : numeros) {
+            soma += num;
+        }
+        System.out.println(soma);
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/Aula52CalculadoraTest05.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/Aula52CalculadoraTest05.java
@@ -1,2 +1,12 @@
-package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;public class Aula52CalculadoraTest05 {
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Aula44Calculadora;
+
+public class Aula52CalculadoraTest05 {
+    public static void main(String[] args) {
+        Aula44Calculadora calculadora = new Aula44Calculadora();
+        int[] numeros = {1, 2, 3, 4, 5};
+        calculadora.somaArray(numeros);
+        calculadora.somaVarArgs(1, 2, 3, 4, 5, 6, 7);
+    }
 }
