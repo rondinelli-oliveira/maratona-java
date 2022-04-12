583ff95900289e9ab173e12a989c192eb556116d
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-12 19:53:05 -0300

    aula-59 - Orientação Objetos - Construtores pt 02 - Sobrecarga.

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Aula58Anime.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Aula58Anime.java
@@ -5,16 +5,25 @@ public class Aula58Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
     public Aula58Anime(String nome, String tipo, int episodios, String genero) {
+        this();
+        System.out.println("Dentor do construtor com argumentos!");
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
     }
 
-    public Aula58Anime() {
+    public Aula58Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        System.out.println("Dentor do construtor com argumentos 2!");
+        this.estudio = estudio;
+    }
 
+    public Aula58Anime() {
+        System.out.println("Dentro do construtor sem argumentos!");
     }
 
     public void imprimeDados() {
@@ -22,6 +31,7 @@ public class Aula58Anime {
         System.out.println(this.tipo);
         System.out.println(this.episodios);
         System.out.println(this.genero);
+        System.out.println(this.estudio);
     }
 
     public String getNome() {
--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/Aula58AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/Aula58AnimeTest01.java
@@ -4,9 +4,8 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Aula58Anime;
 
 public class Aula58AnimeTest01 {
     public static void main(String[] args) {
-        Aula58Anime anime = new Aula58Anime("Cavaleiros do Zoodiaco", "TV", 12, "Ação");
-        Aula58Anime anime2 = new Aula58Anime();
+        Aula58Anime anime = new Aula58Anime("Cavaleiros do Zoodiaco", "TV", 12, "Ação",
+                "Bandai");
         anime.imprimeDados();
-        anime2.imprimeDados();
     }
 }
