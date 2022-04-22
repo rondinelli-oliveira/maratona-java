6d118cbb5e156a8668f52127e703a57d5d77eabd
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-22 15:44:45 -0300

    aula-77 - Orientação Objetos - Modificador final pt 01 - Tipo primitivo.

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/Aula77CarroTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.test;
+
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Aula77Carro;
+
+public class Aula77CarroTest01 {
+    public static void main(String[] args) {
+        Aula77Carro carro = new Aula77Carro();
+        System.out.println(Aula77Carro.VELOCIDADE_LIMITE);
+    }
+}
