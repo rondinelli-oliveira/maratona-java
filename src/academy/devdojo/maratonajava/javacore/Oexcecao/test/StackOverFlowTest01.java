8ced030fbf154c83cfcfe33c4ebb651693138f8f
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-25 20:54:52 -0300

    aula-95 - Exceções pt 01 - Errors.

---
+++ src/academy/devdojo/maratonajava/javacore/Oexcecao/test/StackOverFlowTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Oexcecao.test;
+
+public class StackOverFlowTest01 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void recursividade() {
+        recursividade();
+    }
+}
