a6911bd9ebae988bd836c18d839da973d9d77dcc
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-25 10:28:33 -0300

    aula-82 - Orientação Objetos - Enumeração pt 03 - Sobrescrita de métodos.

--- src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Aula80Cliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Aula80Cliente.java
@@ -1,15 +1,11 @@
 package academy.devdojo.maratonajava.javacore.Kenum.dominio;
 
 public class Aula80Cliente {
-    public enum TipoPagamento {
-        DEBITO, CREDITO
-    }
-
     private String nome;
     private Aula80TipoCliente tipoCliente;
-    private TipoPagamento tipoPagamento;
+    private Aula82TipoPagamento tipoPagamento;
 
-    public Aula80Cliente(String nome, Aula80TipoCliente tipoCliente, TipoPagamento tipoPagamento) {
+    public Aula80Cliente(String nome, Aula80TipoCliente tipoCliente, Aula82TipoPagamento tipoPagamento) {
         this.nome = nome;
         this.tipoCliente = tipoCliente;
         this.tipoPagamento = tipoPagamento;
--- src/academy/devdojo/maratonajava/javacore/Kenum/test/Aula80ClienteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/Aula80ClienteTest01.java
@@ -1,15 +1,18 @@
 package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.Aula80Cliente;
-import academy.devdojo.maratonajava.javacore.Kenum.dominio.Aula80Cliente.TipoPagamento;
+
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.Aula80TipoCliente;
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.Aula82TipoPagamento;
 
 public class Aula80ClienteTest01 {
     public static void main(String[] args) {
-        Aula80Cliente cliente1 = new Aula80Cliente("Rondinelli Oliveira", Aula80TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
-        Aula80Cliente cliente2 = new Aula80Cliente("Flávia Cristina", Aula80TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+        Aula80Cliente cliente1 = new Aula80Cliente("Rondinelli Oliveira", Aula80TipoCliente.PESSOA_FISICA, Aula82TipoPagamento.DEBITO);
+        Aula80Cliente cliente2 = new Aula80Cliente("Flávia Cristina", Aula80TipoCliente.PESSOA_JURIDICA, Aula82TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(Aula82TipoPagamento.DEBITO.calculaDesconto(100));
+        System.out.println(Aula82TipoPagamento.CREDITO.calculaDesconto(100));
     }
 }
