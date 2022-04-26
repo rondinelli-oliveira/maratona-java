5139073295157d00df2b5593c8b248f6270307dd
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-25 23:21:26 -0300

    aula-97 - Exceções pt 03 - Exception.

--- src/academy/devdojo/maratonajava/javacore/Oexcecao/exception/test/Aula97ExceptionTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Oexcecao/exception/test/Aula97ExceptionTest01.java
@@ -1,2 +1,20 @@
-package academy.devdojo.maratonajava.javacore.Oexcecao.exception.test;public class Aula97ExceptionTest01 {
+package academy.devdojo.maratonajava.javacore.Oexcecao.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Aula97ExceptionTest01 {
+    public static void main(String[] args) {
+        criarNovoArquivo();
+    }
+
+    private static void criarNovoArquivo() {
+        File file = new File("arquivo\\teste.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado com sucesso...." + isCriado);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
