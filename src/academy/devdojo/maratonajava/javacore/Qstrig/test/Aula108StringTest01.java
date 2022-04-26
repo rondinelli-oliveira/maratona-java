55c6a875f47529730431cd2c785bb9d45631454a
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-26 19:35:32 -0300

    aula-108 - Classes Utilitárias - Strings pt 01.

---
+++ src/academy/devdojo/maratonajava/javacore/Qstrig/test/Aula108StringTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Qstrig.test;
+
+public class Aula108StringTest01 {
+    public static void main(String[] args) {
+        String nome = "Rondinelli"; //String constant pool
+        String nome2 = "Rondinelli";
+        nome = nome.concat(" Oliveira");
+        System.out.println(nome);
+        System.out.println(nome == nome2);
+        String nome3 = new String("Rondinelli"); //1 variável de referência, 2 objeto do tipo string, 3 uma string no pool de string
+        System.out.println(nome2 == nome3);
+        System.out.println(nome2 == nome3.intern());
+    }
+}
