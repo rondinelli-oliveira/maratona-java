80cc06f21c41e287ddf566aa28fa9913057adbba
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-26 17:42:20 -0300

    aula-106 - Classes Utilitárias - Wrappers pt 01.

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/Aula106WrapperTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Pwrapper.test;
+
+public class Aula106WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'R';
+        boolean booleanP = false;
+
+        Byte byteR = 1;
+        Short shortR = 1;
+        Integer intR = 1;
+        Long longR = 10L;
+        Float floatR = 10F;
+        Double doubleR = 10D;
+        Character charR = 'R';
+        Boolean booleanR = false;
+    }
+}
