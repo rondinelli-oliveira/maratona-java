343934c1393d91d8d075da682e08b0b8dde613f1
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-26 18:06:18 -0300

    aula-107 - Classes Utilitárias - Wrappers pt 02.

--- src/academy/devdojo/maratonajava/javacore/Pwrapper/test/Aula106WrapperTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/Aula106WrapperTest01.java
@@ -13,11 +13,25 @@ public class Aula106WrapperTest01 {
 
         Byte byteR = 1;
         Short shortR = 1;
-        Integer intR = 1;
+        Integer intR = 1; //autoboxing
         Long longR = 10L;
         Float floatR = 10F;
         Double doubleR = 10D;
         Character charR = 'R';
         Boolean booleanR = false;
+
+        int i = intR; //unboxing
+        Integer intR2 = Integer.parseInt("1");
+        boolean verdadeiro = Boolean.parseBoolean(null);
+        System.out.println(verdadeiro);
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
+
     }
 }
