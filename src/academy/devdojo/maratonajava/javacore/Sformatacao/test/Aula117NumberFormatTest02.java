701b1b18ab40e7cd32ca3220f2ff71d907da91eb
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-27 15:56:32 -0300

    117 - Classes Utilitárias - Internacionalização de moeda com Locale

--- src/academy/devdojo/maratonajava/javacore/Sformatacao/test/Aula117NumberFormatTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Sformatacao/test/Aula117NumberFormatTest02.java
@@ -4,25 +4,25 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class Aula116NumberFormatTest01 {
+public class Aula117NumberFormatTest02 {
     public static void main(String[] args) {
         Locale localeBrasil = new Locale("pt", "BR");
         Locale localeJapao = Locale.JAPAN;
         Locale localeItalia = Locale.ITALY;
         Locale localeEstadosUnidos = Locale.US;
         NumberFormat[] numberFormats = new NumberFormat[5];
-        numberFormats[0] = NumberFormat.getInstance();
-        numberFormats[1] = NumberFormat.getInstance(localeBrasil);
-        numberFormats[2] = NumberFormat.getInstance(localeJapao);
-        numberFormats[3] = NumberFormat.getInstance(localeItalia);
-        numberFormats[4] = NumberFormat.getInstance(localeEstadosUnidos);
+        numberFormats[0] = NumberFormat.getCurrencyInstance();
+        numberFormats[1] = NumberFormat.getCurrencyInstance(localeBrasil);
+        numberFormats[2] = NumberFormat.getCurrencyInstance(localeJapao);
+        numberFormats[3] = NumberFormat.getCurrencyInstance(localeItalia);
+        numberFormats[4] = NumberFormat.getCurrencyInstance(localeEstadosUnidos);
         double valor = 100_000_000.2130;
         for (NumberFormat numberFormat : numberFormats) {
-            numberFormat.setMaximumFractionDigits(2);
+            System.out.println(numberFormat.getMaximumFractionDigits());
             System.out.println(numberFormat.format(valor));
         }
 
-        String valorString = "1000.2130";
+        String valorString = "R$ 1000,2130";
         try {
             System.out.println(numberFormats[0].parse(valorString));
         } catch (ParseException e) {
