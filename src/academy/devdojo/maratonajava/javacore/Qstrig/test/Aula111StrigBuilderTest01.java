a9e7524a42ef3bd0e1bae72b9780d8e766a836f6
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-26 22:48:31 -0300

    aula-111 - Classes Utilitárias - Strings pt 04 - StringBuilder.

---
+++ src/academy/devdojo/maratonajava/javacore/Qstrig/test/Aula111StrigBuilderTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Qstrig.test;
+
+public class Aula111StrigBuilderTest01 {
+    public static void main(String[] args) {
+        String nome = "Rondinelli Oliveira";
+        nome.concat(" Evolution Company Technology");
+        nome.substring(0,3);
+        System.out.println(nome);
+        StringBuffer sb = new StringBuffer("Rondinelli Oliveira");
+        sb.append(" Evolution").append(" Company").append(" Technology");
+        sb.reverse();
+        sb.reverse();
+        sb.delete(0,3);
+        System.out.println(sb);
+    }
+}
