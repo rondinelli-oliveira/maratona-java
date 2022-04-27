de6bb03cfab137c5e9d51aa4c3a606777cd90ffa
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-27 09:50:20 -0300

    114 - Classes Utilitárias - DateFormat

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/Aula114DateFormatTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class Aula114DateFormatTest01 {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        DateFormat[] dateFormats = new DateFormat[7];
+        dateFormats[0] = DateFormat.getInstance();
+        dateFormats[1] = DateFormat.getDateInstance();
+        dateFormats[2] = DateFormat.getDateTimeInstance();
+        dateFormats[3] = DateFormat.getDateInstance(DateFormat.SHORT);
+        dateFormats[4] = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        dateFormats[5] = DateFormat.getDateInstance(DateFormat.LONG);
+        dateFormats[6] = DateFormat.getDateInstance(DateFormat.FULL);
+
+        for (DateFormat dateFormat : dateFormats) {
+            System.out.println(dateFormat.format(calendar.getTime()));
+        }
+    }
+}
