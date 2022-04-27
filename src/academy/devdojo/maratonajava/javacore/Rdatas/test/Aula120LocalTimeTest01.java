690f313f400a5e778fd5983535d7b056881676be
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-04-27 17:33:38 -0300

    120 - Classes Utilitárias - LocalTime.

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/Aula120LocalTimeTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+
+public class Aula120LocalTimeTest01 {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.of(23, 32, 12);
+        LocalTime timeNow = LocalTime.now();
+        System.out.println(time);
+        System.out.println(timeNow);
+        System.out.println(timeNow.getHour());
+        System.out.println(timeNow.getMinute());
+        System.out.println(timeNow.getSecond());
+        System.out.println(timeNow.getNano());
+        System.out.println(timeNow.get(ChronoField.CLOCK_HOUR_OF_AMPM));
+        System.out.println(LocalTime.MIN);
+        System.out.println(LocalTime.MAX);
+        System.out.println(LocalTime.MIDNIGHT);
+
+    }
+}
