b4386843be44a4122e3c0263a0af6fee29808f75
rondinelli-oliveira <rondinelliads@gmail.com>
2022-06-13 15:44:21 -0300

    aula-196 - Lambdas pt 02 - Consumer.

---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambidas/test/Aula196LambdaTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.ZZClambidas.test;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Aula196LambdaTest01 {
+    public static void main(String[] args) {
+        List<String> strings = List.of("Rondinelli", "Flavia", "Gabriel", "Felipe");
+        List<Integer> integers = List.of(1, 2, 3, 4, 5);
+        forEach(strings, s -> System.out.println(s));
+        forEach(integers, i -> System.out.println(i));
+    }
+
+    private static <T> void forEach(List<T> list, Consumer<T> consumer) {
+        for (T e : list) {
+            consumer.accept(e);
+        }
+    }
+}
