4abfd3d92eb0b21248c287a73b4915dc7ddb4506
rondinelli-oliveira <rondinelliads@gmail.com>
2022-06-20 17:25:48 -0300

    aula-209 - Streams pt 07 - Reduce pt 01.

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/Aula209StreamTest07.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import java.util.List;
+
+public class Aula209StreamTest07 {
+
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        integers.stream().reduce((x, y) -> x + y).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(0, (x, y) -> x + y));
+
+        integers.stream().reduce(Integer::sum).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(0, Integer::sum));
+
+        System.out.println("--------------------------------");
+
+        integers.stream().reduce((x, y) -> x * y).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(1, (x, y) -> x * y));
+
+        System.out.println("--------------------------------");
+
+        integers.stream().reduce((x, y) -> x > y ? x : y).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(0,Integer::max));
+    }
+}
