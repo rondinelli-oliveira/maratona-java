f2def9792af3e006c92a2dc6948f4acfeff115cc
rondinelli-oliveira <rondinelliads@gmail.com>
2022-07-09 19:27:32 -0300

    aula-234 - Concorrência pt 06 - ArrayBlockingQueue.

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/Aula234BlockingQueueTest01.java
@@ -0,0 +1,37 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class Aula234BlockingQueueTest01 {
+    public static void main(String[] args) throws InterruptedException {
+        BlockingQueue<String> bq = new ArrayBlockingQueue<>(1);
+        bq.put("Rondinelli");
+        System.out.printf("%s added the value of %s\n", Thread.currentThread().getName(), bq.peek());
+        System.out.println("Trying to add another value");
+        new Thread(new RemoveFromQueue(bq)).start();
+        bq.put("Oliveira");
+        System.out.printf("%s added the value of %s\n", Thread.currentThread().getName(), bq.peek());
+    }
+
+    static class RemoveFromQueue implements Runnable {
+        private final BlockingQueue<String> bq;
+
+        public RemoveFromQueue(BlockingQueue<String> bq) {
+            this.bq = bq;
+        }
+
+        @Override
+        public void run() {
+            System.out.printf("%s going to sleep for 5s %n", Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(5);
+                System.out.printf("%s removing value from queue %s%n", Thread.currentThread().getName(), bq.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
